d5009b0de4b5dbc3f9bce5ee5253ef98ebd9d1e3
Sam Carlton <sam@sam.lc>
2017-08-24 13:14:54 -0500

    Redirect based on env variable name

--- index.php
+++ index.php
@@ -15,4 +15,27 @@ if(in_array($_SERVER['REMOTE_ADDR'], $local)){
 	$dotenv->load();
 }
 
-var_dump($_ENV);
+// Get everything after the / in the url ex: http://youtube-playlist-json.dev/PLaa9cZC07ZPFVBqmmNZ4JLM2hb-Ixvtuy
+$tokens = explode('/', $_SERVER['REQUEST_URI']);
+$request_path = $tokens[1];
+
+$url = '';
+
+if( $request_path === '' ){
+	$url = $_ENV['endpoint-index'];
+} else {
+  foreach($_ENV as $key => $var){
+    $is_endpoint  = (0 === strpos($key, 'endpoint-'));
+    $is_path      = (strlen($key) - strlen($request_path) == strpos($key, $request_path));
+
+    if ($is_endpoint && $is_path) {
+      $url = $var;
+    }
+  }
+}
+
+if ($url !== '') {
+    header('Location: ' . $url, true, 302);
+}
+
+exit;
